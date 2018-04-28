0259a5833534791018dfe357d12714bc2711c467
valerija2 <hocus2008@gmail.com>
2018-04-28 10:49:42 +0300

    Initial commit

---
+++ src/test/java/lv/akurss/rss/client/RunnerTest.java
@@ -0,0 +1,17 @@
+package lv.akurss.rss.client;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RunnerTest {
+
+    @Test
+    public void testFormat(){
+        String input = "Test &nbsp;&lt;&gt;";
+        String formatted = Runner.format(input);
+        Assert.assertEquals("\n\n====================\nTest  <>\n===================\n", formatted);
+    }
+
+}
